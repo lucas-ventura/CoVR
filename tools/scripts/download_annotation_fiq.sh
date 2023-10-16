ed1575a637940077a0e6e4d3dc3e9bd3ddb10eb6
Lucas Ventura <lucas.ventura.r@gmail.com>
2023-10-16 11:36:32 +0200

    Add split fiq files

--- tools/scripts/download_annotation_fiq.sh
+++ tools/scripts/download_annotation_fiq.sh
@@ -12,15 +12,24 @@ echo -e "Downloading ${BLUE}fashion-iq Train${NC} annotations..."
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.dress.train.json -q -P annotation/fashion-iq/
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.shirt.train.json -q -P annotation/fashion-iq/
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.toptee.train.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.dress.train.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.shirt.train.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.toptee.train.json -q -P annotation/fashion-iq/
 
 # Download Val annotations
 echo -e "Downloading ${BLUE}fashion-iq Val${NC} annotations..."
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.dress.val.json -q -P annotation/fashion-iq/
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.shirt.val.json -q -P annotation/fashion-iq/
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.toptee.val.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.dress.val.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.shirt.val.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.toptee.val.json -q -P annotation/fashion-iq/
 
 # Download Test annotations
 echo -e "Downloading ${BLUE}fashion-iq Test${NC} annotations..."
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.dress.test.json -q -P annotation/fashion-iq/
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.shirt.test.json -q -P annotation/fashion-iq/
 wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.toptee.test.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.dress.test.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.shirt.test.json -q -P annotation/fashion-iq/
+wget https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.toptee.test.json -q -P annotation/fashion-iq/
