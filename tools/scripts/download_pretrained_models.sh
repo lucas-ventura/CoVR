6ba2794deaf4613b172e7b8183259ae7d01ae8c0
Lucas Ventura <lucas.ventura.r@gmail.com>
2023-10-23 15:03:26 +0200

    Add download links for finetuned checkpoints

--- tools/scripts/download_pretrained_models.sh
+++ tools/scripts/download_pretrained_models.sh
@@ -2,11 +2,49 @@
 
 # Define colors
 BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# Download WebVid-CoVR checkpoint
-dir=outputs/webvid-covr/blip-large/blip-l-coco/tv-False_loss-hnnce_lr-1e-05/good/
-mkdir -p $dir
-echo -e "${BLUE}Downloading WebVid-CoVR checkpoint...${NC}"
-wget "https://drive.google.com/uc?id=18agaaO2RNJP1W86Kls5z6bxp8_B5sVtP&export=download&confirm=t&uuid=2d190ab0-c0d9-4092-9c29-d20a49fffbd9" -q --show-progress -O $dir/ckpt_4.ckpt
+# webvid-covr
+url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/webvid-covr.ckpt"
+dir="outputs/webvid-covr/blip-large/blip-l-coco/tv-False_loss-hnnce_lr-1e-05/good/"
+filename="ckpt_4.ckpt"
 
+# Create the directory if it doesn't exist
+mkdir -p "$dir"
+
+# Check if the file already exists
+if [ -e "$dir/$filename" ]; then
+    echo -e "The ${BLUE}WebVid-CoVR${NC} checkpoint already exists."
+else
+    # Download the file and check if it's downloaded correctly
+    echo -e "Downloading ${BLUE}WebVid-CoVR${NC} checkpoint..."
+    if wget -q --show-progress "$url" -O "$dir/$filename"; then
+        echo -e "${GREEN}Download successful.${NC}"
+    else
+        echo -e "${RED}Download failed.${NC}"
+    fi
+fi
+
+
+# webvid-covr fine-tuned on CIRR
+url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/cirr_ft-covr%2Bgt.ckpt"
+dir="outputs/cirr/blip-large/webvid-covr/tv-False_loss-hnnce_lr-0.0001/base/"
+filename="ckpt_5.ckpt"
+
+# Create the directory if it doesn't exist
+mkdir -p "$dir"
+
+# Check if the file already exists
+if [ -e "$dir/$filename" ]; then
+    echo -e "The ${BLUE}WebVid-CoVR CIRR finetuned${NC} checkpoint already exists."
+else
+    # Download the file and check if it's downloaded correctly
+    echo -e "Downloading ${BLUE}WebVid-CoVR CIRR${NC} finetuned checkpoint..."
+    if wget -q --show-progress "$url" -O "$dir/$filename"; then
+        echo -e "${GREEN}Download successful.${NC}"
+    else
+        echo -e "${RED}Download failed.${NC}"
+    fi
+fi
