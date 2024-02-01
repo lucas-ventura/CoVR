ef7daa0fcdadd74a34f42a8fb43975c03b9e5b9e
Lucas Ventura <lucas.ventura.r@gmail.com>
2024-02-01 16:52:40 +0100

    Add FashionIQ checkpoints

--- tools/scripts/download_pretrained_models.sh
+++ tools/scripts/download_pretrained_models.sh
@@ -6,42 +6,94 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# Function to download a checkpoint
 download_checkpoint() {
     local url="$1"
     local dir="$2"
     local filename="$3"
-    local overwrite="$4" # Added a variable for the "overwrite" option
 
     # Create the directory if it doesn't exist
     mkdir -p "$dir"
 
-    # Check if the file already exists and the "overwrite" option is not provided
-    if [ -e "$dir/$filename" ] && [ "$overwrite" != "overwrite" ]; then
+    # Check if the file already exists
+    if [ -e "$dir/$filename" ]; then
         echo -e "The ${BLUE}$filename${NC} checkpoint already exists in $dir."
+        # Ask the user if they want to overwrite the file, defaulting to 'no'
+        read -p "Do you want to overwrite? [y/N]: " overwrite
+        case $overwrite in
+            [Yy]* ) ;;
+            * ) return ;;
+        esac
+    fi
+
+    # Download the file and check if it's downloaded correctly
+    echo -e "Downloading ${BLUE}$filename${NC} checkpoint..."
+    if wget -q --show-progress "$url" -O "$dir/$filename"; then
+        echo -e "${GREEN}Download successful.${NC}"
     else
-        # Download the file and check if it's downloaded correctly
-        echo -e "Downloading ${BLUE}$filename${NC} checkpoint..."
-        if wget -q --show-progress "$url" -O "$dir/$filename"; then
-            echo -e "${GREEN}Download successful.${NC}"
-        else
-            echo -e "${RED}Download failed.${NC}"
-        fi
+        echo -e "${RED}Download failed.${NC}"
     fi
 }
 
-# webvid-covr
-url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/webvid-covr.ckpt"
-dir="outputs/webvid-covr/blip-large/blip-l-coco/tv-False_loss-hnnce_lr-1e-05/good/"
-filename="ckpt_4.ckpt"
+# Function to display download options
+choose_download_option() {
+    echo "Select the model to download:"
+    echo "1) All"
+    echo "2) WebVid-CoVR"
+    echo "3) CIRR"
+    echo "4) FashionIQ"
+    echo "Press Enter for default (All)"
+    read -p "Enter your choice (1/2/3/4): " choice
+    case $choice in
+        2) download_webvid_covr ;;
+        3) download_cirr ;;
+        4) download_fiq ;;
+        *) download_all ;;
+    esac
+}
 
-# Call the download_checkpoint function with the "overwrite" argument
-download_checkpoint "$url" "$dir" "$filename" "$1"
+# WebVid-CoVR
+download_webvid_covr() {
+    url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/webvid-covr.ckpt"
+    dir="outputs/webvid-covr/blip-large/blip-l-coco/tv-False_loss-hnnce_lr-1e-05/good/"
+    filename="ckpt_4.ckpt"
+    download_checkpoint "$url" "$dir" "$filename"
+}
 
-# webvid-covr fine-tuned on CIRR
-url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/cirr_ft-covr%2Bgt.ckpt"
-dir="outputs/cirr/blip-large/webvid-covr/tv-False_loss-hnnce_lr-0.0001/base/"
-filename="ckpt_5.ckpt"
+# WebVid-CoVR + finetuned on CIRR
+download_cirr() {
+    url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/cirr_ft-covr%2Bgt.ckpt"
+    dir="outputs/cirr/blip-large/webvid-covr/tv-False_loss-hnnce_lr-0.0001/base/"
+    filename="ckpt_5.ckpt"
+    download_checkpoint "$url" "$dir" "$filename"
+}
 
-# Call the download_checkpoint function with the "overwrite" argument
-download_checkpoint "$url" "$dir" "$filename" "$1"
+# WebVid-CoVR + finetuned on FashionIQ
+download_fiq() {
+    # Dress
+    url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/fashioniq-dress-ft_covr.ckpt"
+    dir="outputs/fashioniq-dress/blip-large/webvid-covr/tv-False_loss-hnnce_lr-0.0001/base"
+    filename="ckpt_5.ckpt"
+    download_checkpoint "$url" "$dir" "$filename"
+
+    # Shirt
+    url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/fashioniq-shirt-ft_covr.ckpt"
+    dir="outputs/fashioniq-shirt/blip-large/webvid-covr/tv-False_loss-hnnce_lr-0.0001/base"
+    filename="ckpt_5.ckpt"
+    download_checkpoint "$url" "$dir" "$filename"
+
+    # Toptee
+    url="https://huggingface.co/lucas-ventura/CoVR/resolve/main/fashioniq-toptee-ft_covr.ckpt"
+    dir="outputs/fashioniq-toptee/blip-large/webvid-covr/tv-False_loss-hnnce_lr-0.0001/base"
+    filename="ckpt_5.ckpt"
+    download_checkpoint "$url" "$dir" "$filename"
+}
+
+# all models
+download_all() {
+    download_webvid_covr
+    download_cirr
+    download_fiq
+}
+
+# Main script execution
+choose_download_option
