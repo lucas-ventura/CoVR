7e45d8a0c374dcc6eda607cd5f97a8da1033c026
Lucas Ventura <lucas.ventura.r@gmail.com>
2024-03-15 14:34:15 +0100

    Add image split files to Fashion-IQ annotations

--- tools/scripts/download_annotation_fiq.sh
+++ tools/scripts/download_annotation_fiq.sh
@@ -24,17 +24,26 @@ echo "Checking Fashion-IQ Train annotations..."
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.dress.train.json" "annotation/fashion-iq"
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.shirt.train.json" "annotation/fashion-iq"
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.toptee.train.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.dress.train.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.shirt.train.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.toptee.train.json" "annotation/fashion-iq"
 
 # Download Val annotations
 echo "Checking Fashion-IQ Val annotations..."
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.dress.val.json" "annotation/fashion-iq"
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.shirt.val.json" "annotation/fashion-iq"
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.toptee.val.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.dress.val.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.shirt.val.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.toptee.val.json" "annotation/fashion-iq"
 
 # Download Test annotations
 echo "Checking Fashion-IQ Test annotations..."
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.dress.test.json" "annotation/fashion-iq"
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.shirt.test.json" "annotation/fashion-iq"
 download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/captions/cap.toptee.test.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.dress.test.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.shirt.test.json" "annotation/fashion-iq"
+download_file "https://raw.githubusercontent.com/XiaoxiaoGuo/fashion-iq/master/image_splits/split.toptee.test.json" "annotation/fashion-iq"
 
 python tools/scripts/merge_fiq_annotations.py
